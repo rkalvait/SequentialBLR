6c78fe6972b9c37b0d4451bb0a31a99719ae40bb
Adrian Padin <apadin1@users.noreply.github.com>
2016-06-06 11:14:46 -0400

    Added settings and and fixed wm issues

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import numpy as np
 import os
 import sys
 import threading as th
+from collections import OrderedDict
 
 import matplotlib
 matplotlib.use('TkAgg')
@@ -24,18 +25,28 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 import grapher
 
 
+####################  DEFINITIONS  ####################
+
+settings_file = "settings.txt"
+
+
 ####################  HELPER FUNCTIONS  ####################
 
 # Wrapper function for consistent time formatting
 def time2string(timestamp):
     return dt.datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
 
-# Give the window a title and icon, and tell it to destroy when X is pressed
+# Give the window a title and icon, destroy cleanly when X is pressed
 def initWindow(window, title=" "):
     window.wm_title(title) # Change title
     icon_file = PhotoImage(file = "merit_icon.ppm") # Change icon
     window.tk.call( 'wm', 'iconphoto', window._w, icon_file )
-    window.protocol("WM_DELETE_WINDOW", window.destroy)
+
+    def quit_and_destroy(window):
+        window.quit()
+        window.destroy()
+
+    window.protocol("WM_DELETE_WINDOW", lambda: quit_and_destroy(window))
 
 
 ####################  APPLICATION  ####################
@@ -43,16 +54,15 @@ def initWindow(window, title=" "):
 # Class that describes the application's attributes
 class App(Frame):
 
-
     # Constructor
     def __init__(self, master=None):
-        
+
         Frame.__init__(self, master)
 
         # There are two major frames: the left from contains the dashboard,
         # which displays information and options for the analysis, and the
         # right from is the prediction/error graph.
-        
+
         self.createDashFrame() # Contains the dashboard, including time, power, and settings
         self.createGraphFrame() # Frame which contains power and error graphs
 
@@ -65,11 +75,11 @@ class App(Frame):
 
     # Starting point for the Dashboard Frame
     def createDashFrame(self):
-        
+
         self.dashFrame = Frame(self)
         self.dashFrame.pack(side='left', padx=10, pady=10, fill='both')
         Label(self.dashFrame, text="NextHome Dashboard", font=('bold')).grid(pady=10)
-        
+
         # Timer and power
         self.curtime = int(time.time())
         self.timer = Label(self.dashFrame, text="Current time:\t")
@@ -77,23 +87,17 @@ class App(Frame):
         self.power = Label(self.dashFrame, text="Current usage:\t0.0 kW")
         self.power.grid(sticky=W)
         self.after(200, self.updateTime)
-        
-        # Gap row
-        Label(self.dashFrame, text="").grid()
+
+        Label(self.dashFrame, text="").grid() # Gap row
 
         # Settings display
-        Label(self.dashFrame, text="Settings", font=('bold')).grid(sticky=W)
-        self.datafile = "results.csv"
-        self.update_rate = 60
-        self.datafileLabel = Label(self.dashFrame, text="Data file:  \tresults.csv")
-        self.datafileLabel.grid(sticky=W)
-        self.update_rateLabel = Label(self.dashFrame, text="Update rate:\t60 seconds")
-        self.update_rateLabel.grid(sticky=W)
+        self.settingsFrame = Frame(self.dashFrame)
+        self.settingsFrame.grid(sticky=W)
+        self.getSettings()  # read settings from file
+        self.showSettings() # display settings in settingsFrame
 
-        
-        # Gap row
-        Label(self.dashFrame, text="").grid()
-        
+        Label(self.dashFrame, text="").grid() # Gap row
+
         # Settings button
         self.settings_button = Button(self.dashFrame,
                                       text="Change settings...",
@@ -105,8 +109,8 @@ class App(Frame):
         Label(self.dashFrame, text="").grid()# Gap row
 
         # Graph from file
-        self.graph_button = Button(self.dashFrame, 
-                                   text="Graph From File", 
+        self.graph_button = Button(self.dashFrame,
+                                   text="Graph From File",
                                    command=self.graphFromFile)
         self.graph_button.grid(sticky=W+E)
         self.graph_status = Label(self.dashFrame, text="")
@@ -115,8 +119,8 @@ class App(Frame):
         Label(self.dashFrame, text="").grid()# Gap row
 
         # Start analysis
-        self.analysis_button = Button(self.dashFrame, 
-                                      text="Start Analysis", 
+        self.analysis_button = Button(self.dashFrame,
+                                      text="Start Analysis",
                                       bg='green',
                                       activebackground='green',
                                       command=self.algoStart)
@@ -130,67 +134,118 @@ class App(Frame):
             self.curtime = int(time.time())
             self.timer.configure(text=("Current time:\t" + time2string(time.time())))
         self.after(200, self.updateTime) # Repeat every 200 milliseconds
-        
+
+
+    ####################  SETTINGS  ####################
+
+    # Read in settings from the settings file and put in a dictionary
+    def getSettings(self):
+        infile = open(settings_file, 'rb')
+        self.settings = OrderedDict()
+
+        for line in infile:
+            line = line.split()
+            if len(line) == 0: continue       # Ignore blank lines
+            if line[0] == '#': continue       # Ignore comments
+            self.settings[line[0]] = line[-1]
+
+        infile.close()
+
+
+    # Display settings in the Settings tab
+    def showSettings(self):
+
+        # First delete any existing widgets
+        for wid in self.settingsFrame.winfo_children():
+            wid.destroy()
+
+        Label(self.settingsFrame, text="Settings", font=('bold')).grid(sticky=W)
+        for key in self.settings:
+            Label(self.settingsFrame, text=(key + ":   " + self.settings[key])).grid(sticky=W)
+
 
     # Opens the settings window
     def settingsWindow(self):
 
-        # Create the new window (using Toplevel)
+        # Create a new window and initialize
         settings_window = Toplevel(self)
         initWindow(settings_window, title="Settings")
         Grid.columnconfigure(settings_window, 0, weight=1)
-        
+
         # Disable main window and grey out the button
         settings_window.grab_set()
         self.settings_button.configure(fg='grey')
 
-        # Entry bars for all current settings. Current settings include:
-        # -- update_rate: how often to update graph with new data
-        # -- datafile: file to read data from
-        
-        Label(settings_window, text="Data filename:").grid(row=1, column=0, sticky=W)
-        file_input = Entry(settings_window)
-        file_input.insert(0, self.datafile)
-        file_input.grid(row=1, column=1)
-        
-        Label(settings_window, text="Update rate:").grid(row=0, column=0, sticky=W)
-        update_input = Entry(settings_window)
-        update_input.insert(0, self.update_rate)
-        update_input.grid(row=0, column=1)
+        # Make entry bars for all current settings
+        entry_dict = {}
+        count = 0
+        for key in self.settings:
+            Label(settings_window, text=(key + ":  ")).grid(row=count, column=0, sticky=W)
+            new_entry = Entry(settings_window)
+            new_entry.grid(row=count, column=1, sticky=E)
+            new_entry.insert(0, self.settings[key])
+            entry_dict[key] = new_entry
+            count += 1
 
         # Save changes button
         Button(settings_window,
                text="Save changes",
-               command=(lambda: self.updateSettings(update_input.get(),
-                                                    file_input.get()))
-               ).grid(row=2, columnspan=2, sticky=W+E)
+               command=(lambda: self.updateSettings(entry_dict))
+               ).grid(columnspan=2, sticky=W+E)
 
         # Display a warning if any inputs are incorrect (see "updateSettings")
         self.settings_status = Label(settings_window, text="")
 
         def closeSettings():
             self.settings_button.configure(fg='black')
+            #settings_window.quit()
             settings_window.destroy()
+            print ("closed settings window")
 
         settings_window.protocol("WM_DELETE_WINDOW", closeSettings)
 
-        
-    # Update the current settings
-    def updateSettings(self, update_rate, datafile):
-            
-        # Sanitize inputs
-        try:
-            self.datafile = datafile
-            self.update_rate = int(update_rate)
-        except:
-            self.settings_status.config(text="Invalid entry: try again", fg='red')
-            self.settings_status.grid(row=3, columnspan=2)
-        else:
-            self.update_rateLabel.configure(text="Update rate:\t%d seconds" % self.update_rate)
-            self.datafileLabel.configure(text="Data file:  \t%s" % self.datafile)
-            self.settings_status.config(text="Changes saved", fg='green')
-            self.settings_status.grid(row=3, columnspan=2)
 
+    # Update the current settings
+    def updateSettings(self, entry_dict):
+
+        # TODO: sanitize inputs
+        try:
+            for key in self.settings:
+                value = entry_dict[key].get().strip()
+                assert value != ''
+                self.settings[key] = entry_dict[key].get()
+        except AssertionError:
+            self.settings_status.config(text="Invalid entry for %s: try again" % key, fg='red')
+            self.settings_status.grid(columnspan=2)
+        else:
+            self.writeSettings(self.settings)
+            self.showSettings()
+            self.settings_status.config(text="Changes saved", fg='green')
+            self.settings_status.grid(columnspan=2)
+
+
+    # Write settings out to the settings file
+    def writeSettings(self, settings):
+        outfile = open(settings_file, 'rb')
+        line_list = []
+
+        # Read in current file and make changes line-by-line
+        for line in outfile:
+            line_list.append(line)
+            line = line.split()
+            if len(line) > 0 and line[0] != '#':
+                if line[0] in settings:
+                    line = line[0] + ' : ' + str(settings[line[0]]) + '\n'
+                    line_list[-1] = line
+
+        # Write changes to file
+        outfile.close()
+        outfile = open("settings.txt", 'wb')
+        for line in line_list:
+            outfile.write(line)
+
+
+    ####################  ALGORITHM  ####################
 
     # Start the algorithm
     def algoStart(self):
@@ -217,14 +272,14 @@ class App(Frame):
 
     # Startin point for Graph Frame
     def createGraphFrame(self):
-    
-        self.graphFrame = Frame(self, bg='red')
+
+        self.graphFrame = Frame(self)
         self.graphFrame.pack(side='right', fill='both', expand=True)
         Grid.rowconfigure(self.graphFrame, 0, weight=1)
         Grid.columnconfigure(self.graphFrame, 0, weight=1)
-        
+
         fig = plt.figure() # Create figure
-        
+
         self.graph_predict = fig.add_subplot(211) # Target versus prediction
         self.graph_error = fig.add_subplot(212)   # Error (target - prediction)
 
@@ -265,7 +320,7 @@ class App(Frame):
 
     # Get new data to graph
     def graphFromFile(self):
-        
+
         self.graph_status.configure(text="Reading data from file...")
         self.graph_button.configure(state='disabled', fg='grey')
 
@@ -276,20 +331,21 @@ class App(Frame):
             self.graph_button.configure(state='normal', fg='black')
             return
 
+        # Graph results in a new thread
         self.graph_status.configure(text="Graphing data. Please wait...")
         graph_thread = th.Thread(target=self.graphData, args=(y_target, y_predict, y_time))
         graph_thread.start()
 
-        
+
     # Send given data to the graph
     def graphData(self, y_target, y_predict, y_time):
-    
+
         # First check if y_time is list of datetime strings or UNIX timestamps
         if isinstance(y_time[0], str):
             y_time = [dt.datetime.strptime(t, "%Y-%m-%d %H:%M:%S\n") for t in y_time]
         elif isinstance(y_time[0], float):
             y_time = [dt.datetime.fromtimestamp(t) for t in y_time]
-    
+
         # Calculate the error vector
         y_error = []
         for i in xrange(len(y_target)):
@@ -317,24 +373,27 @@ class App(Frame):
         self.target_line.set_data(y_time, y_target)
         self.error_line.set_data(y_time, y_error)
 
+        #graph_predict.autofmt_xdate()
+        #graph_error.autofmt_xdate()
+
         self.canvas.show()
         plt.tight_layout()
-        
+
         self.graph_status.configure(text="Graphing complete.")
         self.graph_button.configure(state='normal', fg='black')
 
 
 ####################  MAIN EXECUTION  ####################
-        
+
 # Execute the application
 root = Tk()
 initWindow(root, title="NextHome Energy Analysis")
-root.protocol("WM_DELETE_WINDOW", lambda: sys.exit(0))
 
 try:
     app = App(master=root)
     root.mainloop()
 except KeyboardInterrupt:
     print "Exiting on keyboard interrrupt"
-
+    root.quit()
+    root.destroy()
     
