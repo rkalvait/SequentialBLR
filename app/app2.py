6cae8909d210147a2b98bcd54301a2ab223385cc
Adrian Padin <apadin@umich.edu>
2016-06-22 14:58:17 -0400

    Testing new GUI library

--- app/app2.py
+++ app/app2.py
@@ -1,21 +1,52 @@
 import sys
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt4 import QtGui, QtCore
 
-def window():
-   app = QtGui.QApplication(sys.argv)
-   w = QtGui.QWidget()
 
-   '''
-   b = QtGui.QLabel(w)
-   b.setText("Hello World!")
-   w.setGeometry(100,100,200,50)
-   b.move(50,20)
-   '''
+class Window(QtGui.QMainWindow):
 
-   w.setWindowTitle("Hello world!")
-   w.show()
-   sys.exit(app.exec_())
-	
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setGeometry(50, 50, 500, 300)
+        self.setWindowTitle('My PyQt Application')
+        self.setWindowIcon(QtGui.QIcon('merit_icon.ppm'))
+        self.statusBar()
+        self.menu()
+        self.home()
+        self.show()
+
+    def menu(self):
+        extractAction = QtGui.QAction("&GET TO THE CHOPPER", self)
+        extractAction.setShortcut("Ctrl+Q")
+        extractAction.setStatusTip("Leave the app")
+        extractAction.triggered.connect(self.close_application)
+        
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('&File')
+        fileMenu.addAction(extractAction)
+
+        
+    def home(self):
+        close_button = QtGui.QPushButton("Quit", self)
+        close_button.clicked.connect(self.close_application)
+        close_button.resize(close_button.sizeHint())
+        close_button.move(100, 100)
+        
+        extractAction = QtGui.QAction(QtGui.QIcon('merit_icon.ppm'), 'Close', self)
+        extractAction.triggered.connect(self.close_application)
+        
+        self.toolBar = self.addToolBar("Extraction")
+        self.toolBar.addAction(extractAction)
+        
+        
+        
+    def close_application(self):
+        print("Closing down")
+        sys.exit()
+        
+def main():
+    app = QtGui.QApplication(sys.argv)
+    toplevel = Window()
+    sys.exit(app.exec_())
+    
 if __name__ == '__main__':
-   window()
+    main()
