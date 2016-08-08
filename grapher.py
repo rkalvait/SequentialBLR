4d9b8569a39f6029b2b59d24b01f5aef47837dc1
Adrian Padin <apadin@umich.edu>
2016-08-08 12:33:26 -0400

    Updates to graphical interface

--- grapher.py
+++ grapher.py
@@ -9,7 +9,7 @@
 This program graphs files with the following format:
 
 Timestamp,Target,Prediction,Anomaly
-1464763755,9530,26466,0
+1464763755,9530,9683,0
 
 - Timestamp is an integer representing a UTC timestamp
 - Target and Prediction are power values in Watts
@@ -58,7 +58,7 @@ class Settings(dict):
                 if len(line) == 0: continue     # Ignore blank lines
                 key, value = line.split('=')
                 self[key] = value
-                
+
     def save(self):
         """Write settings back to the settings file."""
         with open(self.settingsfile, 'wb') as outfile:
@@ -73,6 +73,7 @@ class ResultsGraph(FigureCanvas):
     """Figure class used for graphing results"""
 
     def __init__(self, parent=None, width=5, height=4, dpi=80):
+        """Create and display an empty graph."""
         self.fig = Figure(figsize=(width, height), dpi=dpi)
         FigureCanvas.__init__(self, self.fig)
         self.setParent(parent)
@@ -106,12 +107,14 @@ class ResultsGraph(FigureCanvas):
         self.updateGeometry()
         self.fig.tight_layout()
         self.draw()
-        
-    # Update the graph using the given data
-    # 'times' should be datetime objects
-    # 'target' should be float values in Watts
-    # 'predict' should be float values in Watts
+
     def graphData(self, times, target, predict):
+        """ Update the graph using the given data
+        'times' should be datetime objects
+        'target' should be float values in Watts
+        'predict' should be float values in Watts
+        """
+
         assert(len(times) == len(target))
         assert(len(times) == len(predict))
 
@@ -139,27 +142,38 @@ class ResultsGraph(FigureCanvas):
         self.fig.tight_layout()
         self.draw()
 
-    # Add a vertical color span to the target-prediction graph
-    # 'start' should be a datetime (preferably in range)
-    # 'duration' should be the width of the span in minutes
-    # 'color' should be a string describing an _acceptable_ color value
     def colorSpan(self, start, duration, color):
+        """Add a vertical color span to the target-prediction graph
+        'start' should be a datetime (preferably in range)
+        'duration' should be the width of the span in minutes
+        'color' should be a string describing an _acceptable_ color value
+        """
+
         end = start + dt.timedelta(minutes=duration)
         span = self.graph_power.axvspan(xmin=start, xmax=end, color=color, alpha=0.2)
-        self.color_spans.append(span)
         self.fig.tight_layout()
         self.draw()
 
-    # Remove all vertical color spans
+    def colorSpans(self, spans):
+        """Add a series of vertical color spans to the graph.
+        'spans' should be a list of tuples of the form: (start, duration, color)"""
+        for span in spans:
+            start = span[0]
+            end = start + dt.timedelta(minutes=span[1])
+            span = self.graph_power.axvspan(xmin=start, xmax=end, color=span[2], alpha=0.2)
+            self.color_spans.append(span)
+        self.fig.tight_layout()
+        self.draw()
+
     def clearSpans(self):
+        """Remove all vertical color spans."""
         for span in self.color_spans:
             span.remove()
         self.color_spans = []
         self.fig.tight_layout()
         self.draw()
 
-        
-        
+
 class ResultsWindow(QtGui.QMainWindow):
 
     """Main application window, creates the widgets and the window"""
@@ -176,22 +190,20 @@ class ResultsWindow(QtGui.QMainWindow):
         self.statusBar()
         main_widget = QtGui.QWidget()
         self.graph_widget = self.graphWidget()
-        self.settings_widget = self.optionsWidget()
+        self.options_widget = self.optionsWidget()
 
         # Add children to layout and set focus to main widget
         layout = QtGui.QHBoxLayout()
-        layout.addWidget(self.settings_widget)
+        layout.addWidget(self.options_widget)
         layout.addWidget(self.graph_widget, stretch=1)
         main_widget.setLayout(layout)
         main_widget.setFocus()
         self.setCentralWidget(main_widget)
         self.show()
-        
+
         # Load the data and graph it
         self.loadFile(RESULTS_FILE)
-        self.updateGraph()
-        
-        
+
     #==================== WIDGET FUNCTIONS ====================#
     # These functions create all the widgets, sub-widgets, etc. of the program.
     # Each function creates a new widget instance, adds all necessary layouts
@@ -209,135 +221,86 @@ class ResultsWindow(QtGui.QMainWindow):
         main_widget.setLayout(layout)
         return main_widget
 
-    '''
-    # Create the settings window below the grapher
-    def settingsWidget(self):
-        main_widget = QtGui.QWidget(self)
-        file_widget = self.fileWidget(main_widget)
-        self.options_widget = self.optionsWidget(main_widget)
-        self.options_widget.setDisabled(True)
-
-        layout = QtGui.QHBoxLayout(main_widget)
-        layout.addWidget(file_widget)
-        layout.addWidget(self.options_widget)
-        main_widget.setLayout(layout)
-        return main_widget
-
-    # Creates the filename bar and browse button
-    def fileWidget(self, parent):
-        main_widget = QtGui.QWidget(parent)
-        layout = QtGui.QGridLayout()
-        
-        file_label = QtGui.QLabel("Results file: ", main_widget)
-        file_label.setFont(QtGui.QFont("Arial", 10, QtGui.QFont.Bold))
-        self.start_label = QtGui.QLabel(" ", main_widget)
-        self.end_label = QtGui.QLabel(" ", main_widget)
-        self.file_edit = QtGui.QLineEdit(main_widget)
-        browse = QtGui.QPushButton('Browse...')
-        browse.clicked.connect(self.browseFile)
-        
-        layout.addWidget(file_label, 0, 0)
-        layout.addWidget(self.file_edit, 1, 0)
-        layout.addWidget(browse, 1, 1)
-        layout.addWidget(self.start_label, 2, 0)
-        layout.addWidget(self.end_label, 3, 0)
-        
-        main_widget.setLayout(layout)
-        return main_widget
-    '''
-
     # Creates the options panel to toggle smoothing and anomalies
     def optionsWidget(self):
         main_widget = QtGui.QWidget(self)
         layout = QtGui.QFormLayout()
-        
+
+        options_label = QtGui.QLabel("Settings:", main_widget)
+        options_label.setFont(QtGui.QFont("Arial", 10, QtGui.QFont.Bold))
+        layout.addRow(options_label)
+
+        self.settings_widget = self.settingsWidget()
+        self.settings_widget.setDisabled(True)
+        layout.addRow(self.settings_widget)
+
+        self.edit_button = QtGui.QPushButton("Edit Settings", main_widget)
+        self.edit_button.clicked.connect(self.editOptions)
+        layout.addRow(self.edit_button)
+
+        reset_button = QtGui.QPushButton("Reset to Default", main_widget)
+        reset_button.clicked.connect(self.resetOptions)
+        layout.addRow(reset_button)
+
+        update_button = QtGui.QPushButton("Load New Data", main_widget)
+        update_button.clicked.connect(lambda: self.loadFile(RESULTS_FILE))
+        layout.addRow(update_button)
+
+        main_widget.setLayout(layout)
+        return main_widget
+
+    def settingsWidget(self):
+        main_widget = QtGui.QWidget(self)
+        layout = QtGui.QFormLayout()
         self.settings = Settings(SETTINGS_FILE)
-        
-        self.options_label = QtGui.QLabel("Options:", main_widget)
-        self.options_label.setFont(QtGui.QFont("Arial", 10, QtGui.QFont.Bold))
-        layout.addRow(self.options_label)
-        
-        # Available options are:
-        # - Smooth data (minutes)
-        # - Show anomalies (or hide)
-        # - Show last X hours
-        # - Update every X minutes
-        # - Reset settings button
-        # - Update now button
-        
+
         update_label = QtGui.QLabel("Update rate (minutes):   ")
         self.update_edit = QtGui.QSpinBox(main_widget)
+        self.update_edit.setRange(1, 100)
+        print self.update_edit.maximum()
+        print self.update_edit.minimum()
         self.update_edit.setValue(int(self.settings['update']))
         layout.addRow(update_label, self.update_edit)
 
         past_label = QtGui.QLabel("Past results to show (hours):   ")
         self.past_edit = QtGui.QSpinBox(main_widget)
+        self.past_edit.setRange(1, 100)
         self.past_edit.setValue(int(self.settings['past']))
         layout.addRow(past_label, self.past_edit)
 
-        self.smooth_box = QtGui.QCheckBox("Smooth data (minutes):    ", main_widget)
+        self.smooth_box = QtGui.QCheckBox("Smooth data (window in minutes):    ", main_widget)
         self.smooth_box.stateChanged.connect(self.smoothToggled)
         self.smooth_edit = QtGui.QSpinBox(main_widget)
-        self.smooth_edit.setValue(0)
+        self.smooth_edit.setRange(1, 100)
+        self.smooth_edit.setValue(int(self.settings['smooth']))
         self.smooth_edit.setDisabled(True)
         layout.addRow(self.smooth_box, self.smooth_edit)
-        
-        self.anomaly_box = QtGui.QCheckBox("Show anomalies", main_widget)
-        self.anomaly_box.stateChanged.connect(self.anomalyToggled)
-        layout.addRow(self.anomaly_box)
-        
-        reset_button = QtGui.QPushButton("Reset Options", main_widget)
-        reset_button.clicked.connect(self.resetOptions)
-        layout.addRow(reset_button)
 
-        update_button = QtGui.QPushButton("Update Now", main_widget)
-        update_button.clicked.connect(self.updateGraph)
-        layout.addRow(update_button)
-        
+        self.anomaly_box = QtGui.QCheckBox("Show anomalies (window in minutes):    ", main_widget)
+        self.anomaly_box.stateChanged.connect(self.anomalyToggled)
+        self.anomaly_edit = QtGui.QSpinBox(main_widget)
+        self.anomaly_edit.setRange(1, 100)
+        self.anomaly_edit.setValue(int(self.settings['anomaly']))
+        self.anomaly_edit.setDisabled(True)
+        layout.addRow(self.anomaly_box, self.anomaly_edit)
+
         main_widget.setLayout(layout)
         return main_widget
-        
+
     #==================== HELPER FUNCTIONS ====================#
     # These functions do the actual work of the program.
     # Most are called in response to an event triggered by the main window, while
     # others are helper functions which perform a simple task.
 
-    '''
-    # Return true if the given filename is valid, false otherwise
-    def checkFilename(self, filename):
-        if filename == '':
-            self.statusBar().showMessage("Error: no file name given")
-            return False
-        elif filename[-4:] != '.csv':
-            self.statusBar().showMessage("Error: file must be '.csv' format")
-            return False
-        return True
-        
-    # Open the file search dialog window and get the resulting filename
-    def browseFile(self):
-        filename = str(QtGui.QFileDialog.getOpenFileName())
-        self.file_edit.setText(filename)
-        if (filename != ''):
-            if (self.anomaly_box.isChecked()):
-                self.anomaly_box.toggle()
-            if (self.smooth_box.isChecked()):
-                self.smooth_box.toggle()
-            self.loadFile()
-            self.canvas.graphData(self.times, self.target, self.predict)
-            self.options_widget.setEnabled(True)
-            self.statusBar().showMessage("Graphing complete.", 5000)
-
-    '''
-    # Load data from the file given by filename
     def loadFile(self, filename):
         """Load in the data from the results file."""
-
         try:
             file = open(filename, 'rb')
-        except:
+        except IOError as error:
             #raise RuntimeError('Bad results filename')
-            print "Error: bad results filename"
-            sys.exit(1)            
+            #print "Error: bad results filename"
+            print repr(error)
+            sys.exit(1)
         else:
             reader = csv.reader(file)
             headers = reader.next()
@@ -356,81 +319,101 @@ class ResultsWindow(QtGui.QMainWindow):
             self.predictions = [float(t) for t in columns[2]]
             if len(headers) >= 4:
                 self.anomalies = [float(t) for t in columns[3]]
-                    
-                    
-            '''
-            self.start_label.setText(
-                "Start time: %s" % dt.datetime.strftime(self.times[0], DATE_FORMAT))
-            self.end_label.setText(
-                "End time: %s" % dt.datetime.strftime(self.times[-1], DATE_FORMAT))
-            '''
-            
-    # Decide what to do based on the state of the smooth checkbox
+
+            self.updateGraph()
+
+    def editOptions(self):
+        """Enables changes to options menu."""
+        self.edit_button.setText("Save Changes and Update Graph")
+        self.edit_button.clicked.disconnect()
+        self.edit_button.clicked.connect(self.saveOptions)
+        self.settings_widget.setEnabled(True)
+
+    def saveOptions(self):
+        """Save changes to options and disable options menu."""
+        self.edit_button.setText("Change Settings")
+        self.edit_button.clicked.disconnect()
+        self.edit_button.clicked.connect(self.editOptions)
+        self.settings['update'] = self.update_edit.value()
+        self.settings['past'] = self.past_edit.value()
+        self.settings['anomaly'] = self.anomaly_edit.value()
+        self.settings['smooth'] = self.smooth_edit.value()
+        self.settings.save()
+        self.settings_widget.setDisabled(True)
+
+        print "options saved"
+        self.updateGraph()
+
+    # Reset the settings to default and draw the original graph
+    def resetOptions(self):
+        self.update_edit.setValue(5)
+        self.past_edit.setValue(24)
+        self.smooth_edit.setValue(1)
+        self.anomaly_edit.setValue(1)
+        self.smooth_box.setCheckState(QtCore.Qt.Unchecked)
+        self.anomaly_box.setCheckState(QtCore.Qt.Unchecked)
+        self.saveOptions()
+
     def smoothToggled(self, state):
+        """Enable or disable data smoothing based on 'state'."""
         if state == QtCore.Qt.Checked:
             self.smooth_edit.setEnabled(True)
         else:
             self.smooth_edit.setDisabled(True)
-            self.smooth_edit.setText('0')
-            
-    # Decide what to do based on the state of the anomaly checkbox
+
     def anomalyToggled(self, state):
+        """Enable or disable anomaly bars based on 'state'."""
         if state == QtCore.Qt.Checked:
+            self.anomaly_edit.setEnabled(True)
+        else:
+            self.anomaly_edit.setDisabled(True)
+
+    def updateGraph(self):
+        """Graph the pre-loaded data and add any desired features."""
+        if self.smooth_box.isChecked():
+            smoothing_window = min(self.smooth_edit.value(), len(self.times))
+            self.canvas.graphData(
+                self.times,
+                movingAverage(self.targets, smoothing_window),
+                movingAverage(self.predictions, smoothing_window))
+        else:
+            self.canvas.graphData(self.times, self.targets, self.predictions)
+
+        if self.anomaly_box.isChecked():
             self.showAnomalies()
         else:
             self.canvas.clearSpans()
-    
-    # Decide what to do based on the state of the smooth checkbox
-    def updateGraph(self):
-        try:
-            smoothing_window = int(self.smooth_edit.text())
-        except ValueError:
-            error_window = QtGui.QErrorMessage(self)
-            error_window.showMessage("Smoothing window must be integer value.")
-        else:
-            if smoothing_window > 0:
-                self.canvas.graphData(
-                    self.times,
-                    movingAverage(self.targets, smoothing_window),
-                    movingAverage(self.predictions, smoothing_window))
-            else:
-                self.canvas.graphData(self.times, self.targets, self.predictions)
-            self.statusBar().showMessage("Graphing complete.", 5000)
-                
-    # Reset the settings to default and draw the original graph
-    def resetOptions(self):
-        self.smooth_box.setCheckState(QtCore.Qt.Unchecked)
-        self.anomaly_box.setCheckState(QtCore.Qt.Unchecked)
-        #self.canvas.clearSpans()
-        self.canvas.graphData(self.times, self.target, self.predict)
 
-    # Draw colored bars to show regions where anomalies happened
     def showAnomalies(self):
-        self.settings_widget.setDisabled(True)
-        loading_win = LoadingWindow()
+        """Draw colored bars to show regions where anomalies happened."""
+        loading_window = LoadingWindow()
+        self.options_widget.setDisabled(True)
+        self.canvas.clearSpans() #Clear any existing spans
 
-        start = self.times[0]
-        dur = 10
+        start = 0
+        dur = self.anomaly_edit.value()
         level1 = 0
         level2 = dur / 3.0
         level3 = level2 * 2
-        self.canvas.clearSpans() #Clear any existing spans
+        spans = []  # List of color spans
 
-        count = 0
-        anomaly_count = 0
-        for i in self.anomalies:
-            anomaly_count += self.anomalies[count]
-            if ((count+1) % dur) == 0:
-                if anomaly_count >   level3: self.canvas.colorSpan(start, 1.0/6.0, 'red')
-                elif anomaly_count > level2: self.canvas.colorSpan(start, 1.0/6.0, 'orange')
-                elif anomaly_count > level1: self.canvas.colorSpan(start, 1.0/6.0, 'green')
-                anomaly_count = 0
-                start = self.times[count]
-                QtGui.QApplication.processEvents()
-            count += 1
-        
-        self.settings_widget.setEnabled(True)
-        loading_win.close()
+        while start < len(self.anomalies):
+            #Count number of anomalies and choose a corresponding color
+            start_time = self.times[start]
+            count = sum(self.anomalies[start:(start + dur)])
+            if count > 0:
+                if count > level3:
+                    spans.append((start_time, dur, 'red'))
+                elif count > level2:
+                    spans.append((start_time, dur, 'orange'))
+                else:
+                    spans.append((start_time, dur, 'green'))
+            start += dur
+
+        print "number of spans: ", len(spans)
+        self.canvas.colorSpans(spans)
+        self.options_widget.setEnabled(True)
+        loading_window.close()
 
 
 class LoadingWindow(QtGui.QDialog):
@@ -441,7 +424,6 @@ class LoadingWindow(QtGui.QDialog):
         super(LoadingWindow, self).__init__(None, QtCore.Qt.WindowStaysOnTopHint)
         self.setWindowTitle(' ')
         self.setWindowIcon(QtGui.QIcon(ICON_FILE))
-
         layout = QtGui.QVBoxLayout()
         layout.addWidget(QtGui.QLabel("Calculating. Please wait...", self))
         progress = QtGui.QProgressBar(self)
@@ -450,7 +432,7 @@ class LoadingWindow(QtGui.QDialog):
         layout.addWidget(progress, stretch=1)
         self.setLayout(layout)
         self.show()
-        
+
 
 #==================== MAIN ====================#
 def main(argv):
